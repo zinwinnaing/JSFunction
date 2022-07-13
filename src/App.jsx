64fa6f499676c77d86c01d2c1d8e9a9cf7869336
zinwin996 <zinwinnaing996@gmail.com>
2022-07-13 21:43:48 +0630

    fixed in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { testArray } from "./data/data";
 import "./App.css";
 const App = () => {
@@ -20,13 +20,48 @@ const App = () => {
   };
 
   getGroupHaveSameLetter(testArray);
+
+  const [text, setText] = useState("");
+  const [removeParentHease, setRemoveParentHease] = useState("");
+  const removeParentHeaseAndRevese = (string) => {
+    let str;
+    str = string.replace(/[()]/g, "");
+    setRemoveParentHease(str);
+  };
   return (
-    <div>
+    <div className="wapper">
       <div>
         {Object.values(sameLetterGroup).map((d, index) => {
           return <div key={index}>{d.sort().join("-")}</div>;
         })}
       </div>
+      <br />
+      <br />
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            removeParentHeaseAndRevese(text);
+          }}
+        >
+          Change
+        </button>
+        <button
+          onClick={() => {
+            setText("");
+            setRemoveParentHease("");
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div>Result: &nbsp;{removeParentHease}</div>
     </div>
   );
 };
